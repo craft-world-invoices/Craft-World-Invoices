7e12b13f2e5bdcffa0f7c742751944fef20d05a8
syedsulaimanshah <syedsulaimanshah2006@gmail.com>
2026-06-07 17:46:28 +1000

    Initial Commit

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'invoice-craft-v1';
+const assets = ['./index.html'];
+
+// Install Service Worker
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Fetch Assets
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
